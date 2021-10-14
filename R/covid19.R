6a404473647b2f9ab099b11b2b954184312aecd3
Jørn I. Halvorsen <jorn.halvorsen@gmail.com>
2021-10-14 14:09:24 +0200

    delcomments

--- R/covid19.R
+++ R/covid19.R
@@ -11,12 +11,3 @@ plot19ts <- function(sel_cou=NULL,covid19df=NULL, yvar='hosp'){
 			labs(title=x,"Day during the year", y=yvar)
 	})
 }
-### data(package='MSB104TimeSeriesJIH')
-#library(ggplot2)
-#library(dplyr)
-#covidts <- MSB104TimeSeriesJIH::COVID19
-#sel_cou <- c('NOR','ITA','SWE','GBR','ISR','FIN','CZE','ESP','USA','CAN','SVK')
-#covhos <- plot19ts(sel_cou=sel_cou,covid19df=covidts,yvar='hosp')
-#covhos <- plot19ts(sel_cou=sel_cou,covid19df=covidts,yvar='ma_deaths')
-#covhos <- plot19ts(sel_cou=sel_cou,covid19df=covidts,yvar='ma_deaths_perc')
-#gridExtra::grid.arrange(grobs=covhos)
