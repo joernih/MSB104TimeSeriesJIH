ae7ec99396d33182b6ee4ab26579e56e3afd0b4e
Jørn I. Halvorsen <jorn.halvorsen@gmail.com>
2021-10-14 11:32:20 +0200

    med etternavn

--- R/covid19.R
+++ R/covid19.R
@@ -5,16 +5,18 @@ plot19ts <- function(sel_cou=NULL,covid19df=NULL, yvar='hosp'){
 	
 	c_cdf <- sel_cou %>% purrr::map(function(x,df_c=covid19df){
 		out <- df_c %>% dplyr::filter(id==x)
-		gout <- ggplot(out, aes(x=dayofyear,y=hosp, color=year)) + 
+		gout <- ggplot(out, aes_string(x='dayofyear',y=yvar, color='year')) + 
 			geom_point() + 
 			geom_line() + 
-			labs(title=x,"Day during the year", y="Hospitlalized")
+			labs(title=x,"Day during the year", y=yvar)
 	})
 }
-## data(package='MSB104TimeSeriesJIH')
+### data(package='MSB104TimeSeriesJIH')
 #library(ggplot2)
 #library(dplyr)
 #covidts <- MSB104TimeSeriesJIH::COVID19
 #sel_cou <- c('NOR','ITA','SWE','GBR','ISR','FIN','CZE','ESP','USA','CAN','SVK')
 #covhos <- plot19ts(sel_cou=sel_cou,covid19df=covidts,yvar='hosp')
+#covhos <- plot19ts(sel_cou=sel_cou,covid19df=covidts,yvar='ma_deaths')
+#covhos <- plot19ts(sel_cou=sel_cou,covid19df=covidts,yvar='ma_deaths_perc')
 #gridExtra::grid.arrange(grobs=covhos,)
--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,7 @@
 #' This is a hello function with a name argument
 #' @export
-hello <- function(name=NULL) {
-  tekst <- paste("Hello world! My name is", name)
+hello <- function(name=NULL,ettern=NULL) {
+  tekst <- paste("Hello world! My name is", name, ettern)
   print(tekst)
 }
 
